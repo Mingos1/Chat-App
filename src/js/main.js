a5c4183d65b4156ee3b50b949684cb2409770db2
Mingos <mingos1337@gmail.com>
2020-10-30 04:01:19 -0400

    Testing out CSS

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+let socket = new WebSocket('wss://127.0.0.1:5500');
+
+document.forms.publish.onsubmit = function() {
+    let outgoingMessage = this.message.value;
+
+    socket.send(outgoingMessage);
+    return false;
+}
+
+socket.onmessage = function(event) {
+    let message = event.data;
+    console.log(message);
+
+    let messageElem = document.createElement('div');
+    messageElem.textContent = message;
+    document.getElementById('chat-window').append(messageElem);
+
+}
